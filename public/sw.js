f2fd1ea7faaca91c369eb94d52817ad2989ddd3b
dzdarren0611-del <dzdarren0611@gmail.com>
2026-06-28 01:21:54 +0800

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE = "cycle-tracker-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/src/main.jsx",
+  "/src/App.jsx",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
